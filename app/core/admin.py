b129bd7338993b6555611801706ccfe26c4bbe40
Maaz Bin Tahir <diagaaos@gmail.com>
2020-02-10 15:55:39 +0500

    Store hash passwords, changed User Admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
 
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
 from . import models
 
+class UserAdmin(BaseUserAdmin):
+    ordering = ['email']
+    list_display = ['email']
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'name', 'title', 'avatar')}),
+        (('Permissions'),
+         {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (('Important dates'), {'fields': ('last_login',)})
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
 # User
-admin.site.register(models.User)
+admin.site.register(models.User, UserAdmin)
 
 # Forum
 admin.site.register(models.Question)
