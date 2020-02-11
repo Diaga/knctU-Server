a223cfd4101e74378b492e986357f25d8b9510e8
Maaz Bin Tahir <diagaaos@gmail.com>
2020-02-11 18:18:27 +0500

    Implemented ChatRoom View Set

--- app/chat/urls.py
+++ app/chat/urls.py
@@ -1,5 +1,26 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import Route
+
+from app.urls import router
+from . import views
 
 app_name = 'chat'
 
-urlpatterns = []
+router.routes += [
+    # ChatRoom View Route
+    Route(
+        url=r'^chat{trailing_slash}room{trailing_slash}$',
+        mapping={
+            'get': 'view_chat_room'
+        },
+        name='chat_room-view',
+        detail=False,
+        initkwargs={'suffix': 'View'}
+    ),
+]
+
+router.register('chat', views.ChatRoomViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
--- app/chat/views.py
+++ app/chat/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins, status
+from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from core.models import ChatRoom
+from . import serializers
+
+
+class ChatRoomViewSet(viewsets.GenericViewSet):
+    """View set for ChatRoom model"""
+
+    authentication_classes = [TokenAuthentication, ]
+
+    permission_classes = [IsAuthenticated, ]
+
+    queryset = ChatRoom.objects.all()
+
+    serializer_class = serializers.ChatRoomSerializer
+
+    def get_queryset(self):
+        """Enforce scope"""
+        return self.request.user.chat_rooms.all()
+
+    def view_chat_room(self, request, *args, **kwargs):
+        """Return scoped chat rooms"""
+        serializer = self.get_serializer(
+            self.get_queryset(), many=True
+        )
+        return Response(serializer.data, status=status.HTTP_200_OK)
