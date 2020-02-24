4b7dce3b2053f3d5bcc7dc7df3f30a917cd22249
Maaz Bin Tahir <diagaaos@gmail.com>
2020-02-24 23:59:06 +0500

    Updated sesttings for remote db

--- app/app/settings.py
+++ app/app/settings.py
@@ -79,8 +79,12 @@ ASGI_APPLICATION = 'app.routing.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'app',
+        'USER': 'postgres',
+        'PASSWORD': 'somethingsecretpassword',
+        'HOST': 'knctu.com',
+        'PORT': '5432',
     }
 }
 
